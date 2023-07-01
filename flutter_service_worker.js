efd8735c4ad1b9a1834f64febfc9c3ae679c7775
Anurag Verma <anurag.verma.ug20@nsut.ac.in>
2023-07-01 17:09:50 +0530

    bug fixes

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -65,9 +65,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "dfe288dacfb134a83fde6eb3efd87e6d",
-"/": "dfe288dacfb134a83fde6eb3efd87e6d",
-"main.dart.js": "bcde46e00cc2b7e522fc52794995e7fa",
+"index.html": "08a2316de33315446a5dfb60f875ce31",
+"/": "08a2316de33315446a5dfb60f875ce31",
+"main.dart.js": "00e4107a045e3cba83bcac60367ba212",
 "manifest.json": "f393956267731452763ed2f701d5444d",
 "version.json": "9f9cbb400ca9e5d449968d9b44c421d2"
 };
